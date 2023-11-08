b12ab53b1e007026039ab41998397851a092430e
DennyHsu <denny890413@gmail.com>
2023-11-08 20:00:16 +0800

    修改Binary indexed tree為區間查詢版本

--- BIT.cpp
+++ BIT.cpp
@@ -1,41 +1,74 @@
 /*
- Binary Indexed Tree
+ Binary Indexed Tree with range update & range query
 
  build by TM Hsu 2023/01/31
 */
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 using ll = long long;
-class BIT {
+
+class BIT{
 public:
-  vector<ll> arr;
-  BIT(int n) { arr.resize(n + 1, 0); }
+    vector<ll> arr1,arr2;
+    BIT(vector<ll> &v,int n){
+        arr1.resize(n+5,0);
+        arr2.resize(n+5,0);
 
-  void update(int n, int delta = 1) {
-    while (n < arr.size()) {
-      arr[n] += delta;
-      n += n & (-n);
+        for(int i=0; i<v.size(); i++){
+            update(i+1,v[i]);
+        }
     }
-  }
+    void update(ll idx,ll val){
+        int x = idx;
+        for(int i=idx; i<arr1.size(); i+= i & (-i)){
+            arr1[i] += val;
+            arr2[i] += (val*(x-1));
+        }
+    }
+    // add range [l,r] by val (1-indexed)
+    void update(ll l,ll r,ll val){
+        update(l,val);
+        update(r+1,-val);
+    }
 
-  ll query(int n) {
-    ll sum = 0;
-    while (n > 0) {
-      sum += arr[n];
-      n -= n & (-n);
+    ll query(ll idx){
+        int x = idx;
+        ll ans = 0;
+        for(int i=idx; i>0; i-= i & (-i)){
+            ans += arr1[i] * x - arr2[i];
+        }
+        return ans;
     }
-    return sum;
-  }
+    // return range sum [l,r]  (1-indexed)
+    ll query(ll l,ll r){
+        return query(r) - query(l-1);
+    }
 };
 
-int main() {
-  BIT bit(10);
-  bit.update(5);
-  bit.update(3);
-
-  cout << bit.query(4) << endl;
-  bit.update(8);
-  cout << bit.query(10) << endl;
-  return 0;
+int main(){
+    int n,q;
+    cin>>n>>q;
+    ll prev = 0;
+    vector<ll> v;
+    for(int i=0; i<n; i++){
+        ll s;
+        cin>>s;
+        v.push_back(s - prev);
+        prev = s;
+    }
+    BIT bit(v,v.size());
+    for(int i=0; i<q; i++){
+        ll a;
+        cin>>a;
+        if (a == 1){
+            ll l,r,v;
+            cin>>l>>r>>v;
+            bit.update(l,r,v);
+        }
+        else{
+            ll k;
+            cin>>k;
+            cout<<bit.query(k,k)<<endl;
+        }
+    }
 }
